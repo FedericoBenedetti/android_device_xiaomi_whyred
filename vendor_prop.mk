696a61d532610b1cc47bfc7efc00dcc9628a4465
Federico Benedetti <fede.benedetti9@gmail.com>
2019-03-05 13:57:51 -0800

    whyred: clean-up some props

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -182,12 +182,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.sensors.sta_detect=true \
     ro.vendor.sensors.mot_detect=true
 
-# Camera
-PRODUCT_PROPERTY_OVERRIDES += \
-	persist.camera.HAL3.enabled=1 \
-    vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
-    vendor.camera.hal1.packagelist=com.skype.raider,com.whatsapp,com.snapchat.android,com.instagram.android
-
 #disable UBWC for camera
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.preview.ubwc=0 \
