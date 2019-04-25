The-Great-One/Syberia_device_xiaomi_whyred

whyred: doze: Make AOD preference not persistent
Change-Id: I9d80e94de02d35c85765302a01b53020824dd0ff
Signed-off-by: rashedsahaji <asifrashed2@gmail.com>

parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -72,6 +72,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
 
         mAlwaysOnDisplayPreference = (SwitchPreference) findPreference(DozeUtils.ALWAYS_ON_DISPLAY);
         mAlwaysOnDisplayPreference.setEnabled(dozeEnabled);
+        mAlwaysOnDisplayPreference.setChecked(DozeUtils.isAlwaysOnEnabled(getActivity()));
         mAlwaysOnDisplayPreference.setOnPreferenceChangeListener(this);
 
         PreferenceCategory pickupSensorCategory = (PreferenceCategory) getPreferenceScreen().

FedericoBenedetti/android_device_xiaomi_whyred

doze/src/org/lineage/settings/doze/DozeSettingsFragment.java
@@ -67,6 +67,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements OnPrefer
 
         mAlwaysOnDisplayPreference = (SwitchPreference) findPreference(Utils.ALWAYS_ON_DISPLAY);
         mAlwaysOnDisplayPreference.setEnabled(dozeEnabled);
+        mAlwaysOnDisplayPreference.setChecked(Utils.isAlwaysOnEnabled(getActivity()));
         mAlwaysOnDisplayPreference.setOnPreferenceChangeListener(this);
 
         PreferenceCategory tiltSensorCategory =